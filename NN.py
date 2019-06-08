e89c39680f5f49c1d77ca950a7a16ab2dfe8f661
Marcel <oefele-marcel@outlook.de>
2019-06-08 22:44:10 +0200

    NN update

--- NN.py
+++ NN.py
@@ -1,8 +1,12 @@
 import tensorflow as tf
 print(tf.__version__)
 from tensorflow import keras
+from keras.models import load_model
+from tensorflow.python.keras.callbacks import TensorBoard
 import pandas
 import pickle
+from time import time
+
 
 tf.test.is_gpu_available()
 
@@ -12,12 +16,10 @@ y_train=pickle.load(open('y_train.pkl','rb'))
 x_test=pickle.load(open('x_test.pkl','rb'))
 y_test=pickle.load(open('y_test.pkl','rb'))
 
-print(x_train)
-
 
 classifier = keras.Sequential()
 #First Hidden Layer
-classifier.add(keras.layers.Dense(1000, activation='sigmoid', kernel_initializer='random_normal', input_dim=x_train.shape[1]))
+classifier.add(keras.layers.Dense(1500, activation='sigmoid', kernel_initializer='random_normal', input_dim=x_train.shape[1]))
 
 #Second  Hidden Layer
 classifier.add(keras.layers.Dense(500, activation='sigmoid', kernel_initializer='random_normal'))
@@ -25,14 +27,21 @@ classifier.add(keras.layers.Dense(500, activation='sigmoid', kernel_initializer=
 #Output Layer
 classifier.add(keras.layers.Dense(201, activation='sigmoid', kernel_initializer='random_normal'))
 
-#Compiling the neural network
-classifier.compile(optimizer ='adam',loss=tf.losses.sigmoid_cross_entropy, metrics=['cosine_proximity'])
+#Instanciate Tensorboard
+tensorboard=TensorBoard(log_dir='logs/{}'.format(time()))
 
-classifier.fit(x_train,y_train,epochs=3,batch_size=10)
+#Compiling the neural network
+classifier.compile(optimizer ='adam',loss=tf.keras.losses.BinaryCrossentropy(), metrics=['binary_accuracy'])
+
+classifier.fit(x_train,y_train,epochs=50,batch_size=10, callbacks=[tensorboard])
 
 
 classifier.save('classifier.h5')
 
 y_pred=classifier.predict_proba(x_test)
-print(y_pred[1])
-print(y_test[1])
+print(y_pred[0])
+print(y_test[0])
+
+classifier=load_model('classifier.h5')
+
+classifier.fit(x_train,y_train,epochs=100,batch_size=10, callbacks=[tensorboard])
